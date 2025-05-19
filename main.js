9ffd944ea0d42e35316ce22e662df6bb21dcc5fd
ddg16 <ddg16@duke.edu>
2025-05-19 19:18:02 -0400

    Replaced p5.js microphone with the standard Web Audio API microphone

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
-let mic;
-let volume;
+let audioContext, analyser, dataArray;
+let volume = 0;
+let micStarted = false;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    mic = new p5.AudioIn();
-    mic.start();
+    startMic();
+    createCanvas(400, 400);
+    textAlign(CENTER, CENTER);
+    textSize(18);
 }
 
-
 function draw() {
-    volume = mic.getLevel() * 255;
-    console.log("volume = " + volume);
-    background(220);
-    circle(200, 200, volume);
+    background(240);
+
+    if (!micStarted) {
+        return;
+    }
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    // Compute volume as RMS
+    let sumSquares = 0;
+    for (let i = 0; i < dataArray.length; i++) {
+        const val = dataArray[i] - 128;
+        sumSquares += val * val;
+    }
+    volume = Math.sqrt(sumSquares / dataArray.length) * 4;
+
+    // Draw a circle that changes with volume
+    fill(100, 180, 255);
+    circle(width / 2, height / 2, volume + 100);
+}
+
+async function startMic() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    audioContext = new(window.AudioContext || window.webkitAudioContext)();
+    const source = audioContext.createMediaStreamSource(stream);
+
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 256;
+    const bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+
+    source.connect(analyser);
+    micStarted = true;
 }
